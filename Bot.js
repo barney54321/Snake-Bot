87e8dbba98d282a0f2657463c1c7f8040db2d6db
barney54321 <andrewesteban1999@gmail.com>
2019-07-15 10:57:48 +1000

    Added fruit

--- Bot.js
+++ Bot.js
@@ -1,3 +1,50 @@
 class Bot {
-    
+    constructor(ctx, cvs) {
+        this.side = 10;
+        this.ctx = ctx;
+        this.cvs = cvs;
+        this.velX = 1;
+        this.velY = 0;
+        this.body = [[0,0]];
+        this.live = true;
+    }
+
+    draw() {
+        for (var i = this.body.length - 1; i >= 0; i--) { 
+            this.ctx.fillStyle = "#000000";
+            this.ctx.fillRect(this.body[i][0], this.body[i][1], this.side, this.side);
+            // this.ctx.fillStyle = "#ffffff";
+            // this.ctx.strokeRect(this.x + 20, this.y, this.side, this.side);
+        }
+
+    }
+
+    click() {
+        let currX = this.body[this.body.length - 1][0];
+        let currY = this.body[this.body.length - 1][1];
+        this.body.shift();
+        var newBox = [];
+        if (this.velX == 1) {
+            newBox.push(currX + this.side);
+            newBox.push(currY);
+        } else if (this.velX == -1) {
+            newBox.push(currX - this.side);
+            newBox.push(currY);
+        } else if (this.velY == 1) {
+            newBox.push(currX);
+            newBox.push(currY + this.side);
+        } else {
+            newBox.push(currX);
+            newBox.push(currY - this.side);
+        }
+
+        for (var i = 0; i < this.body.length; i++) {
+            if (this.body[i][0] == newBox[0] && this.body[i][1] == newBox[1]) {
+                this.live = false;
+                break;
+            }
+        }
+
+        this.body.push(newBox);
+    }
 }
--- Snake.js
+++ Snake.js
@@ -1,2 +1,15 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
+
+var bot = new Bot(ctx, cvs);
+var fruit = new Fruit();
+
+function runner() {
+
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
+    bot.draw();
+    bot.click();
+    requestAnimationFrame(runner);
+}
+
+runner();

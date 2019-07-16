7f280a601464103a8fbdc4ddf529dbbfe47ac5b4
barney54321 <andrewesteban1999@gmail.com>
2019-07-16 18:33:08 +1000

    Trying new method with array

--- Bot.js
+++ Bot.js
@@ -1,15 +1,23 @@
 class Bot {
     constructor(ctx, cvs) {
-        this.side = 10;
         this.ctx = ctx;
         this.cvs = cvs;
-        this.velX = 0;
-        this.velY = 0;
-        this.body = [[(Math.floor((Math.random() * cvs.width) / 10)) * 10, (Math.floor((Math.random() * cvs.height) / 10)) * 10]];
-        this.live = true;
-        this.fitness = 0;
-        this.length = 2;
-        this.timeLeft = 200;
+
+        this.game = [];
+        for (var i = 0; i < 20; i++) {
+            var row = [];
+            for (var j = 0; j < 20; j++) {
+                row.push(0);
+            }
+            this.game.push(row);
+        }
+
+        this.x = 10;
+        this.y = 10;
+        this.game[this.y][this.x] = 2;
+        this.fruitX = Math.floor(Math.random() * 20);
+        this.fruitY = Math.floor(Math.random() * 20);
+        this.game[this.fruitY][this.fruitX] = -1;
 
         // From 6 to 12
         this.wih = [];
@@ -36,167 +44,28 @@ class Bot {
     }
 
     draw() {
-        for (var i = this.body.length - 1; i >= 0; i--) { 
-            this.ctx.fillStyle = "#000000";
-            this.ctx.fillRect(this.body[i][0], this.body[i][1], this.side, this.side);
-            // this.ctx.fillStyle = "#ffffff";
-            // this.ctx.strokeRect(this.x + 20, this.y, this.side, this.side);
+        for (var i = 0; i < 20; i++) {
+            for (var j = 0; j < 20; j++) {
+                if (this.game[i][j] == 0) {
+                    ctx.fillStyle = "#ffffff";
+                } else if (this.game[i][j] == 1) {
+                    ctx.fillStyle = "#000000";
+                } else if (this.game[i][j] == 2) {
+                    ctx.fillStyle = "#0000ff";
+                } else {
+                    ctx.fillStyle = "#ff0000";
+                }
+                ctx.fillRect(i * 10, j * 10, 10, 10);
+            }
         }
-
     }
 
     calculate(fruit) {
-
-        var x = this.body[this.body.length - 1][0];
-        var y = this.body[this.body.length - 1][1];
-
-        // INPUTS: up, down, left, right, fruit, fruit
-        var up = y;
-        for (var i = 0; i < this.body.length - 1; i++) {
-            if (this.body[i][0] == x && this.body[i][1] < y) {
-                var oldUp = up;
-                up = y - (this.body[i][1] + this.side);
-                if (oldUp < up) {
-                    up = oldUp;
-                }
-            }
-        }
-
-        var down = this.cvs.height - y - this.side;
-        for (var i = 0; i < this.body.length - 1; i++) {
-            if (this.body[i][0] == x && this.body[i][1] > y) {
-                var oldUp = up;
-                up = this.body[i][1] + this.side - y;
-                if (oldUp < up) {
-                    up = oldUp;
-                }
-            }
-        }
-
-        var left = x;
-        for (var i = 0; i < this.body.length - 1; i++) {
-            if (this.body[i][1] == y && this.body[i][0] < x) {
-                var oldLeft = left;
-                left = x - (this.body[i][0] + this.side);
-                if (oldLeft < left) {
-                    left = oldLeft;
-                }
-            }
-        }
-
-        var right = this.cvs.width - x - this.side;
-        for (var i = 0; i < this.body.length - 1; i++) {
-            if (this.body[i][1] == y && this.body[i][0] > x) {
-                var oldLeft = left;
-                left = this.body[i][0] + this.side - x;
-                if (oldLeft < left) {
-                    left = oldLeft;
-                }
-            }
-        }
-
-        var fruitX = fruit.x - this.body[this.body.length - 1][0];
-        if (fruitX > 0) {
-            fruitX = 1;
-        } else if (fruitX < 0) {
-            fruitX = -1;
-        }
-        var fruitY = fruit.y - this.body[this.body.length - 1][1];
-        if (fruitY > 0) {
-            fruitY = 1;
-        } else if (fruitY < 0) {
-            fruitY = -1;
-        }
-
-        var inputs = transpose([up, down, left, right, fruitX, fruitY]);
-        var hiddenOutput = matrixMultiply(this.wih, inputs);
-        var hiddenResult = applySigmoid(hiddenOutput);
-        var outputs = matrixMultiply(this.who, hiddenResult);
-        var result = applySigmoid(outputs);
-
-        console.table(inputs)
-
-        if (result[0][0] > 0.5) {
-            if (this.velX == -1) {
-                this.live = false;
-            } else {
-                this.velX = 1;
-                this.velY = 0;
-                this.fitness += 10;
-            }
-        } else if (result[1][0] > 0.5) {
-            if (this.velX == 1) {
-                this.live = false;
-            } else {
-                this.velX = -1;
-                this.velY = 0;
-                this.fitness += 10;
-            }
-        } else if (result[2][0] > 0.5) {
-            if (this.velY == -1) {
-                this.live = false;
-            } else {
-                this.velX = 0;
-                this.velY = 1;
-                this.fitness += 10;
-            }
-        } else if (result[3][0] > 0.5) {
-            if (this.velY == 1) {
-                this.live = false;
-            } else {
-                this.velX = 0;
-                this.velY = -1;
-                this.fitness += 10;
-            }            
-        }
 
     }
 
     click(fruit) {
 
-        this.calculate(fruit);
-        let currX = this.body[this.body.length - 1][0];
-        let currY = this.body[this.body.length - 1][1];
-        if (this.length == this.body.length) {
-            this.body.shift();
-        }
-        var newBox = [];
-        if (this.velX == 1) {
-            newBox.push(currX + this.side);
-            newBox.push(currY);
-        } else if (this.velX == -1) {
-            newBox.push(currX - this.side);
-            newBox.push(currY);
-        } else if (this.velY == 1) {
-            newBox.push(currX);
-            newBox.push(currY + this.side);
-        } else {
-            newBox.push(currX);
-            newBox.push(currY - this.side);
-        }
-
-        for (var i = 0; i < this.body.length; i++) {
-            if (this.body[i][0] == newBox[0] && this.body[i][1] == newBox[1]) {
-                this.live = false;
-                break;
-            }
-        }
-
-        if (newBox[0] < 0 || newBox[0] >= this.cvs.width) {
-            this.live = false;
-        }
-
-        if (newBox[1] < 0 || newBox[1] >= this.cvs.height) {
-            this.live = false;
-        }
-
-        this.timeLeft -= 1;
-        if (this.timeLeft < 0) {
-            this.live = false;
-        }
-
-        this.body.push(newBox);
-        this.fitness += 1;
     }
 }
 
--- Snake.js
+++ Snake.js
@@ -3,103 +3,17 @@ var ctx = cvs.getContext("2d");
 var info = document.getElementById("num");
 var infoNum = document.getElementById("bot");
 
-var bots = [];
-for (var i = 0; i < 500; i++) {
-    bots.push(new Bot(ctx, cvs));
-}
+var bot = new Bot(ctx, cvs);
 
-var num = 0;
-var generation = 0;
+function draw() {
 
-var fruit = new Fruit(ctx, cvs);
-
-function botCompare(a, b) {
-    return b.fitness - a.fitness;
-}
-
-function evolve() {
-
-    bots.sort(botCompare);
-    generation += 1;
-
-    var nextGen = [];
-
-    for (var i = 0; i < 250; i++) {
-        var newBot = new Bot(ctx, cvs);
-        newBot.wih = bots[i].wih;
-        newBot.who = bots[i].who;
-        nextGen.push(newBot);
-    }
-
-    for (var i = 0; i < 20; i++) {
-        for (var j = 0; j < 10; j++) {
-            var newBot = new Bot(ctx, cvs);
-            newBot.wih = crossover(bots[i].wih, bots[j].wih)[0];
-            newBot.who = crossover(bots[i].who, bots[j].who)[0];
-            nextGen.push(newBot);
-        }
-    }
-
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 5; j++) {
-            var r1 = Math.floor(Math.random() * 250);
-            var newBot = new Bot(ctx, cvs);
-            newBot.wih = crossover(bots[i].wih, bots[r1].wih)[0];
-            newBot.who = crossover(bots[i].who, bots[r1].who)[0];
-            nextGen.push(newBot);
-        }
-    }
-
-    for (var i = 0; i < 10; i++) {
-        nextGen.push(new Bot(ctx, cvs));
-    }
-
-    // for (var i = 0; i < 50; i++) {
-    //     var r1 = Math.floor(Math.random() * 50);
-    //     var r2 = Math.floor(Math.random() * 50);
-    //     var newBot = new Bot(ctx, cvs);
-    //     newBot.wih = crossover(bots[r1].wih, bots[r2].wih)[0];
-    //     newBot.who = crossover(bots[r1].who, bots[r2].who)[0];
-    //     nextGen.push(newBot);
-    // }
-
-    for (var i = 100; i < 500; i++) {
-        if (Math.random() > 0.6) {
-            nextGen[i].wih = mutate(nextGen[i].wih);
-            nextGen[i].who = mutate(nextGen[i].who);
-        }
-    }
-
-    num = 0;
-    bots = nextGen;
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
+    bot.draw();
 }
 
 function runner() {
 
-    ctx.clearRect(0, 0, cvs.width, cvs.height);
-    bots[num].draw();
-    fruit.draw();
-
-
-    bots[num].click(fruit);
-    if (bots[num].body[bots[num].body.length - 1][0] == fruit.x && bots[num].body[bots[num].body.length - 1][1] == fruit.y) {
-        bots[num].fitness += 200;
-        fruit = new Fruit(ctx, cvs);
-        bots[num].length += 1;
-        bots[num].timeLeft += 1000;
-    }
-
-    if (bots[num].live == false) {
-        fruit = new Fruit(ctx, cvs);
-        num += 1;
-        if (num >= bots.length) {
-            evolve();
-            num = 0;
-        }
-    }
-
-    info.innerHTML = generation;
-    infoNum.innerHTML = num;
+    draw();
 
     requestAnimationFrame(runner);
 }
